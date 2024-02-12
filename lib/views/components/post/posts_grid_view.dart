27bd240fcc0b347faa2bffd6242fb27b67999a42
ashp8 <ashishira521@gmail.com>
2024-02-13 00:02:42 +0600

    Post Grid and thumbnail view

---
+++ lib/views/components/post/posts_grid_view.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/widgets.dart';
+import 'package:instant_gram/state/posts/models/post.dart';
+import 'package:instant_gram/views/components/post/post_thumbnail.dart';
+
+class PostGridView extends StatelessWidget {
+  final Iterable<Post> posts;
+  const PostGridView({
+    super.key,
+    required this.posts,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+      padding: const EdgeInsets.all(8.0),
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 3,
+        mainAxisSpacing: 8.0,
+        crossAxisSpacing: 8.0,
+      ),
+      itemCount: posts.length,
+      itemBuilder: (context, index) {
+        final post = posts.elementAt(index);
+        return PostThumbnailView(
+          post: post,
+          onTapped: () {},
+        );
+      },
+    );
+  }
+}
